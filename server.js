e0af757cbcc58e80fac0886a6223ae8cfbd735a5
mbrianna1170 <brianna.miller09@yahoo.com>
2021-07-23 22:45:39 -0500

    set up PORT

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 const notes = require('./Develop/db/db.json');
 
@@ -9,6 +10,6 @@ app.get('/api/notes', (req, res) => {
 });
 
 // To start server 
-app.listen(3001, () => {
-    console.log('API server now on port 3001!');
+app.listen(PORT, () => {
+    console.log('API server now on ${PORT}!');
 });
